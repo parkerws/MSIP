d343cec228f769a288d6ecd4eff86b435db09c49
William Parker <47265263+parkerws@users.noreply.github.com>
2020-04-04 13:18:31 -0400

    Created basic task tracker functionality

---
+++ MSIP_App/MSIP_App/Models/Tasks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MSIP_App
+{
+    public class Tasks
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Title must contain at least two characters.")]
+        [MaxLength(200, ErrorMessage = "Title must contain a maximum of 200 characters.")]
+        public string Title { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Due Date")]
+        public DateTime DueDate { get; set; }
+
+        
+        public User TaskOwner { get; set; }
+
+        [DisplayName("Complete")]
+        public bool IsComplete { get; set; }
+
+
+    }
+}
