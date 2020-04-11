b708334b12c03bc44e2b1aa6db49c6753ea2e4ca
William Parker <47265263+parkerws@users.noreply.github.com>
2020-04-11 15:40:29 -0400

    task edit

---
+++ MSIP_App/MSIP_App/Models/TasksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSIP_App.Models
+{
+    public class TasksViewModel
+    {
+        public Tasks Tasks { get; set; }
+        public IEnumerable<Tasks> TaskList { get; set; }
+    }
+}
