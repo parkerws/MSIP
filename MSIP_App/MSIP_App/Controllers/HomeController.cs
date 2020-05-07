5413dae230a7a190064210c3b7b16d277cc63e91
Diego Marquez <diegomarquezlive@live.com>
2020-05-07 11:24:17 -0400

    About Page new!

--- MSIP_App/MSIP_App/Controllers/HomeController.cs
+++ MSIP_App/MSIP_App/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace MSIP_App.Controllers
             return View();
         }
 
+        public IActionResult MSIP()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
